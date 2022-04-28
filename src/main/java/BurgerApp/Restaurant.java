1db720ccc7b4a644fff4a6554c6cadd141526633
jMelendez-ing <74319582+jMelendez-ing@users.noreply.github.com>
2022-04-27 23:01:02 -0500

    Adding service package and Uptating Main class

--- src/main/java/BurgerApp/Restaurant.java
+++ src/main/java/BurgerApp/Restaurant.java
@@ -1,8 +1,15 @@
 package BurgerApp;
 
+import domain.Dish;
+import services.DishDirector;
+
 public class Restaurant {
 
     public static void main(String[] args) {
 
+        DishDirector director = new DishDirector(new BurgerDishBuilder());
+        director.create();
+        Dish myDish = director.getDish();
+        System.out.println("The dish is :" + myDish.getPrice() + " COP");
     }
 }
